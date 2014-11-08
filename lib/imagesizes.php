4c72ed0b16a90d7367602c1eeeed64f778d0bcd4
denovo <pete.richardson82@gmail.com>
2014-11-08 21:48:07 +0000

    add basic picturefill implmentation plugin

--- lib/imagesizes.php
+++ lib/imagesizes.php
@@ -8,4 +8,21 @@ add_image_size('fullscreen', 1920, 9999);
 
 
 
+
+// function picturefill_script() {
+//     wp_enqueue_script( 'picturefill', get_stylesheet_uri() . '/js/picturefill_2.2.0.min.js', '', '', true );
+// }
+
+// add_action( 'wp_enqueue_scripts', 'picturefill_script' );
+
+
+
+// // Get Image Alt - Used for Picturefill
+// function get_img_alt( $image ) {
+//     $img_alt = trim( strip_tags( get_post_meta( $image, '_wp_attachment_image_alt', true ) ) );
+//     return $img_alt;
+// }
+
+
+
 ?>
