d87b7dc5ad963cbd405848862d368bedc91ff1fc
denovo <pete.richardson82@gmail.com>
2014-12-22 20:14:09 +0000

    add short news titles option for the homepage

--- templates/content-homepage.php
+++ templates/content-homepage.php
@@ -7,7 +7,7 @@
 <?php
 
   // heading for news and events
-  echo '<h2 class="heading__page-title">Calendar, News or Gestures</h2>';
+  echo '<h2 class="heading__page-title">Calendar, News or Gestures</h2><div class="panel__news-events">';
 
   //  Left Col News/Events
 	$args = array(
@@ -15,56 +15,97 @@
       'meta_key' => 'news_date', // name of custom field
 		  'orderby' => 'meta_value_num',
 		  'order' => 'DESC',
-	    'posts_per_page' => '-1',
-      'meta_query' => array(
-        'relation' => 'AND',
-        array(
-          'key' => 'show_in_column',
-          'value' => 'Left',
-          'compare' => '='
-        )
-      )
+	    'posts_per_page' => '-1'
 	);
 	$query = new WP_Query( $args );
 
 
 	if ( $query->have_posts() )
 	{
-	
-    // start the left-col list
-    echo '<ul class="list__news_events __left-col medium-6 columns no-pad-l">';
 
+    // newsevents list
+    // echo '<ul class="list__news_events medium-6 columns no-pad-l">';
+    echo '<ul class="list__news_events">';
     $year_check = ''; // set blank year check at beginning - this will be filled with current year after each news post iteration
 
         // loop through left news posts
-        while ( $query->have_posts() ) : $query->the_post(); 
+        while ( $query->have_posts() ) : $query->the_post();
         ?>
 
-          <!-- format post date into readable format and set variable to compare with the year_check variable.
-    			will add an horiz rule to separate years -->
         	<?php
+          // format post date into readable format and set variable to compare with the year_check variable.
+          // will add an horiz rule to separate years
         		$date = DateTime::createFromFormat('Ymd', get_field('news_date'));
     				$p_date = $date->format('M Y');
     				$p_year = $date->format('Y');
     			?>
 
-          <!-- insert horiz rule between posts if they are in different years -->
+
           <?php
+            //  add class to put a top border rule between posts if they are in different years
           	if ($p_year != $year_check) {
           		if ($year_check != '') {
-          			echo '<hr class="news_separator"/>';
+          			// echo '<hr class="news_separator"/>';
+                $new_year = true;
           		}
+              else{
+                $new_year = false;
+              }
+              // update current year
           		$year_check = $p_year;
           	}
           ?>
 
-        <li class="list_item__news" id="post-<?php the_ID(); ?>">
 
-          	<a href="<?php the_permalink(); ?>">
-          		<?php
-          			echo $p_date . ': '; the_title();
-          		?>
-          	</a>
+            <?php
+              if($new_year) { ?>
+                <li class="list_item__news new_year" id="post-<?php the_ID(); ?>">
+
+            <?php
+                 // reset end of section after outputting a divider line
+                 $new_year = false;
+              } else {
+            ?>
+                <li class="list_item__news" id="post-<?php the_ID(); ?>">
+            <?php
+              } // END if($new_year)
+
+              $chosen_post_type = get_field('type_of_post');
+
+              $news_title = get_field('short_news_title');
+
+              if (empty($news_title)) {
+                $news_title = the_title();
+              }
+
+              switch ( $chosen_post_type) {
+                case 'Title Only':
+                  ?>
+                      <span><?php echo $p_date . ': '; echo $news_title ?></span>
+                  <?php
+                  break;
+
+                case 'Link Only':
+                  $the_content_link = get_field('content_link');
+                  ?>
+                      <a href="<?php echo $the_content_link['url'] ?>"><?php echo $p_date . ': '; echo $news_title ?></a>
+                  <?php
+                  break;
+
+                case 'Regular Post':
+                  ?>
+                      <a href="<?php the_permalink(); ?>#post-content"> <?php echo $p_date . ': '; echo $news_title ?> </a>
+                  <?php
+                break;
+
+                default:
+                ?>
+                  <a href="<?php the_permalink(); ?>#post-content"> <?php echo $p_date . ': '; echo $news_title ?> </a>
+                <?php
+                break;
+              }
+
+            ?>
         </li>
 
 
@@ -75,84 +116,20 @@
 
     wp_reset_postdata();
 
-
-     //  Right Col News/Events
-  $args = array(
-      'post_type' => 'news',
-      'meta_key' => 'news_date', // name of custom field
-      'orderby' => 'meta_value_num',
-      'order' => 'DESC',
-      'posts_per_page' => '-1',
-      'meta_query' => array(
-        'relation' => 'AND',
-        array(
-          'key' => 'show_in_column',
-          'value' => 'Right',
-          'compare' => '='
-        )
-      )
-  );
-  $query = new WP_Query( $args );
-
-
-  if ( $query->have_posts() )
-  {
-  
-    // start the left-col list
-    echo '<ul class="list__news_events __right-col medium-6 columns no-pad-l">';
-
-    $year_check = ''; // set blank year check at beginning - this will be filled with current year after each news post iteration
-
-        // loop through left news posts
-        while ( $query->have_posts() ) : $query->the_post(); 
-        ?>
-
-          <!-- format post date into readable format and set variable to compare with the year_check variable.
-          will add an horiz rule to separate years -->
-          <?php
-            $date = DateTime::createFromFormat('Ymd', get_field('news_date'));
-            $p_date = $date->format('M Y');
-            $p_year = $date->format('Y');
-          ?>
-
-          <!-- insert horiz rule between posts if they are in different years -->
-          <?php
-            if ($p_year != $year_check) {
-              if ($year_check != '') {
-                echo '<hr class="news_separator"/>';
-              }
-              $year_check = $p_year;
-            }
-          ?>
-
-        <li class="list_item__news" id="post-<?php the_ID(); ?>">
-
-            <a href="<?php the_permalink(); ?>">
-              <?php
-                echo $p_date . ': '; the_title();
-              ?>
-            </a>
-        </li>
-
-
-
-        <?php endwhile;
-        echo '</ul>'; // close .list__news_events.__right-col
-  }
-
-    wp_reset_postdata();
-
-
   ?>
 
-  <div class="panel__bio">
+ </div> <!-- end .panel__news-events -->
+
+<hr/>
+
+  <div class="panel__bio medium-12 columns no-pad-l">
 
     <?php
 
-      $bioPageID = 12;
+      $bioPageID = 176;
 
       // output Bio Page Title
-    	echo "<h2>" . get_the_title( $bioPageID ) . "</h2>";
+    	echo "<h2>Bio</h2>";
 
         // Output Bio Page content
       echo apply_filters('the_content', get_post_field('post_content', $bioPageID));
