71e409d2ecd598445dc115c54fdd3768cfa6cb6c
denovo <pete.richardson82@gmail.com>
2014-12-31 18:07:12 +0000

    update 404 page

--- 404.php
+++ 404.php
@@ -1,13 +1,14 @@
 <?php get_template_part('templates/page', 'header'); ?>
+<div class="small-16 medium-12 medium-offset-2 columns">
+	<div data-alert class="alert-box warning">
+	  <?php _e('Looks like the page you were trying to view does not exist.', 'roots'); ?>
+	</div>
 
-<div data-alert class="alert-box warning">
-  <?php _e('Looks like the page you were trying to view does not exist.', 'roots'); ?>
+	<p><?php _e('It looks like this was the result of either:', 'roots'); ?></p>
+	<ul>
+	  <li><?php _e('a mistyped address', 'roots'); ?></li>
+	  <li><?php _e('an out-of-date link', 'roots'); ?></li>
+	</ul>
+
+	<?php get_search_form(); ?>
 </div>
-
-<p><?php _e('It looks like this was the result of either:', 'roots'); ?></p>
-<ul>
-  <li><?php _e('a mistyped address', 'roots'); ?></li>
-  <li><?php _e('an out-of-date link', 'roots'); ?></li>
-</ul>
-
-<?php get_search_form(); ?>
